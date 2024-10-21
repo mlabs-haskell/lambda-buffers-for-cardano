4746b9656aa2a5fba35f7c201875f53a588b6afc
jared <>
2024-10-21 16:38:18 -0600

    Shuffled test suite structure s.t. it generalizes over the CLI to produce TxInfo's suitable for building transactions with

--- transactions/demo-tx-village/tests/common/mod.rs
+++ transactions/demo-tx-village/tests/common/mod.rs
@@ -1,17 +1,23 @@
 //! Common functionality for writing tests. Namely:
+//! - Running an integration test suite
 //! - Wrappers for calling the `demo-tx-village` CLI to serialize and deserialize inputs / outputs
 //! to a Rust type
 //! - Setting up the plutip testnet along with other dependencies
 use assert_cmd::Command;
 use lbf_demo_config_api::demo::config::{Config, Script};
 use lbf_demo_plutus_api::demo::plutus::{EqDatum, EqRedeemer, Product, Record, Sum};
+use lbf_demo_plutus_api::demo::request::{ClaimRequest, LockRequest, Request};
+use lbf_demo_plutus_api::demo::response::Response;
 use lbr_prelude::json::Json;
+use num_bigint::BigInt;
+use plutus_ledger_api::v1::transaction::POSIXTime;
 use plutus_ledger_api::v2::address::{Address, Credential};
 use plutus_ledger_api::v2::crypto::LedgerBytes;
 use plutus_ledger_api::v2::script::ValidatorHash;
 use plutus_ledger_api::v2::script::{MintingPolicyHash, ScriptHash};
 use plutus_ledger_api::v2::value::{AssetClass, CurrencySymbol, TokenName};
 use std::io::Write;
+use std::time::SystemTime;
 use tokio::fs;
 use tx_bakery::utils::key_wallet::KeyWallet;
 use tx_bakery::utils::script::ScriptOrRef;
@@ -23,8 +29,322 @@ use tx_bakery_ogmios::{
 use tx_bakery_plutip::{Plutip, PlutipConfigBuilder};
 use url::Url;
 
+/// Given an executable, say `myExecutable` which conforms to the CLI of
+///
+/// a. `myExecutable build-tx-info lock --request <request-filepath>` produces a `Response` suitable
+///   for storing an EqDatum at an Eq validator
+///
+/// b. `myExecutable build-tx-info claim --request <request-filepath>` produces a `Response` suitable
+///   for claiming an EqDatum at an Eq validator
+///
+/// this runs an integration test which:
+///
+/// 1. Runs a.
+/// 2. Runs b. s.t. the claimiing of the Eq validator tests if the redeemer and the datum are the
+///    same
+///
+/// 3. Runs a.
+///
+/// 4. Runs b. s.t. the claimiing of the Eq validator tests if the redeemer and the datum are NOT the
+///    same
+pub async fn run_integration_test(executable: &str, config_path: &str) {
+    let script = read_script(config_path).await.as_validator();
+
+    let ((example_eq_datum_a, example_eq_redeemer_a), (example_eq_datum_b, example_eq_redeemer_b)) =
+        setup_test_data(script.0.clone());
+
+    let (plutip, _ogmios_launcher, _ogmios) = setup_plutip_test().await;
+
+    let network = plutip.get_network();
+
+    let (skey_path, key_wallet, wallet_addr) = get_the_wallet(&network).await;
+
+    // Create the UTxO for the lock command (storing the datum)
+    // -------------------------------
+    {
+        eprintln!("Storing EqDatum A @ EqV");
+        let change_utxos =
+            demo_tx_village_query_utxos(config_path, &network, wallet_addr.clone(), None);
+
+        let current_time = SystemTime::now()
+            .duration_since(SystemTime::UNIX_EPOCH)
+            .unwrap()
+            .as_millis();
+
+        let lock_response = cli_lock_response(
+            executable,
+            config_path,
+            Request {
+                fee_inputs: change_utxos,
+                change_address: key_wallet.get_change_addr(),
+                current_time: POSIXTime(BigInt::from(current_time)),
+                request: LockRequest {
+                    eq_datum: example_eq_datum_a.clone(),
+                },
+            },
+        );
+        let tx_hash = match lock_response {
+            lbf_demo_plutus_api::demo::response::Response::Result(result) => {
+                demo_tx_village_build_and_submit(
+                    config_path,
+                    &network,
+                    skey_path.to_str().unwrap(),
+                    result.tx_info,
+                )
+            }
+            lbf_demo_plutus_api::demo::response::Response::Error(err) => {
+                panic!("{:?}", err)
+            }
+        };
+
+        eprintln!("Successfully stored EqDatum A @ EqV with {:?}", tx_hash);
+    }
+
+    // Create the tx for claiming the locked datum
+    // -----------------------------------------
+    {
+        eprintln!("Checking if EqDatum A is the same as the one previously stored (it should be)");
+
+        let change_utxos =
+            demo_tx_village_query_utxos(config_path, &network, wallet_addr.clone(), None);
+
+        let eq_validator_addr = demo_tx_village_eq_validator_address(config_path, &network);
+
+        let utxos_for_datum = demo_tx_village_query_utxos(
+            config_path,
+            &network,
+            eq_validator_addr.clone(),
+            Some(example_eq_datum_a),
+        );
+
+        let utxo_for_datum = &utxos_for_datum[0];
+
+        let current_time = SystemTime::now()
+            .duration_since(SystemTime::UNIX_EPOCH)
+            .unwrap()
+            .as_millis();
+
+        let claim_response = cli_claim_response(
+            executable,
+            config_path,
+            Request {
+                fee_inputs: change_utxos,
+                change_address: key_wallet.get_change_addr(),
+                current_time: POSIXTime(BigInt::from(current_time)),
+                request: ClaimRequest {
+                    locked_utxo: utxo_for_datum.clone(),
+                    eq_redeemer: example_eq_redeemer_a,
+                },
+            },
+        );
+        let tx_hash = match claim_response {
+            lbf_demo_plutus_api::demo::response::Response::Result(result) => {
+                demo_tx_village_build_and_submit(
+                    config_path,
+                    &network,
+                    skey_path.to_str().unwrap(),
+                    result.tx_info,
+                )
+            }
+            lbf_demo_plutus_api::demo::response::Response::Error(err) => {
+                panic!("{:?}", err)
+            }
+        };
+
+        eprintln!(
+            "Successfully checked that they are indeed the same in transaction {:?}",
+            tx_hash
+        );
+    }
+
+    // Create the UTxO for the lock command (storing the datum)
+    // -------------------------------
+    {
+        eprintln!("Storing EqDatum B @ EqVal");
+        let change_utxos =
+            demo_tx_village_query_utxos(config_path, &network, wallet_addr.clone(), None);
+
+        let current_time = SystemTime::now()
+            .duration_since(SystemTime::UNIX_EPOCH)
+            .unwrap()
+            .as_millis();
+
+        let lock_response = cli_lock_response(
+            executable,
+            config_path,
+            Request {
+                fee_inputs: change_utxos,
+                change_address: key_wallet.get_change_addr(),
+                current_time: POSIXTime(BigInt::from(current_time)),
+                request: LockRequest {
+                    eq_datum: example_eq_datum_b.clone(),
+                },
+            },
+        );
+        let tx_hash = match lock_response {
+            lbf_demo_plutus_api::demo::response::Response::Result(result) => {
+                demo_tx_village_build_and_submit(
+                    config_path,
+                    &network,
+                    skey_path.to_str().unwrap(),
+                    result.tx_info,
+                )
+            }
+            lbf_demo_plutus_api::demo::response::Response::Error(err) => {
+                panic!("{:?}", err)
+            }
+        };
+        eprintln!("Successfully stored EqDatum B with {:?}", tx_hash);
+    }
+
+    // Create the tx for claiming the locked datum
+    // -----------------------------------------
+    {
+        eprintln!(
+            "Checking if Eq Datum A is different to the one previously stored (it should be)"
+        );
+
+        let change_utxos =
+            demo_tx_village_query_utxos(config_path, &network, wallet_addr.clone(), None);
+
+        let eq_validator_addr = demo_tx_village_eq_validator_address(config_path, &network);
+
+        let utxos_for_datum = demo_tx_village_query_utxos(
+            config_path,
+            &network,
+            eq_validator_addr.clone(),
+            Some(example_eq_datum_b),
+        );
+
+        let utxo_for_datum = &utxos_for_datum[0];
+
+        let current_time = SystemTime::now()
+            .duration_since(SystemTime::UNIX_EPOCH)
+            .unwrap()
+            .as_millis();
+
+        let claim_response = cli_claim_response(
+            executable,
+            config_path,
+            Request {
+                fee_inputs: change_utxos,
+                change_address: key_wallet.get_change_addr(),
+                current_time: POSIXTime(BigInt::from(current_time)),
+                request: ClaimRequest {
+                    locked_utxo: utxo_for_datum.clone(),
+                    eq_redeemer: example_eq_redeemer_b,
+                },
+            },
+        );
+        let tx_hash = match claim_response {
+            lbf_demo_plutus_api::demo::response::Response::Result(result) => {
+                demo_tx_village_build_and_submit(
+                    config_path,
+                    &network,
+                    skey_path.to_str().unwrap(),
+                    result.tx_info,
+                )
+            }
+            lbf_demo_plutus_api::demo::response::Response::Error(err) => {
+                panic!("{:?}", err)
+            }
+        };
+
+        eprintln!(
+            "Successfully check that they are indeed different in transaction {:?}",
+            tx_hash
+        );
+    }
+}
+
+/// Calls a CLI interface with the provided executable to produce a `Response` suitable for creating a lock tx.
+///
+/// NOTE(jaredponn) October 21, 2024: the idea is that in the other supported LB languages, there'd
+/// be an executable, say `myExecutable` which conforms to the CLI of
+/// `myExecutable build-tx-info lock --request <request-filepath>`
+/// to build a `Response` suitable for tx-village.
+pub fn cli_lock_response(
+    executable: &str,
+    config_path: &str,
+    request: Request<LockRequest>,
+) -> Response<()> {
+    let mut temp_file = tempfile::NamedTempFile::new().unwrap();
+
+    let json_string_request = request.to_json_string();
+
+    let _ = temp_file.write(json_string_request.as_bytes()).unwrap();
+
+    let output = Command::new(executable)
+        .env("DEMO_CONFIG", config_path)
+        .arg("build-tx-info")
+        .arg("lock")
+        .arg("--request")
+        .arg(temp_file.path().to_str().unwrap())
+        .output()
+        .unwrap();
+
+    let stdout_contents = String::from_utf8(output.stdout.clone()).unwrap();
+
+    if !output.status.success() {
+        panic!(
+            "{} failed with stderr: {}",
+            executable,
+            String::from_utf8(output.stderr.clone()).unwrap()
+        );
+    }
+
+    let result = Json::from_json_string(&stdout_contents).unwrap();
+
+    temp_file.close().unwrap();
+
+    result
+}
+
+/// Calls a CLI with the provided executable to produce a `Response` suitable for creating a claim tx
+///
+/// NOTE(jaredponn) October 21, 2024: the idea is that in the other supported LB languages, there'd
+/// be an executable, say `myExecutable` which conforms to the CLI of
+/// `myExecutable build-tx-info claim --request <request-filepath>`
+/// to build a `Response` suitable for tx-village.
+pub fn cli_claim_response(
+    executable: &str,
+    config_path: &str,
+    request: Request<ClaimRequest>,
+) -> Response<()> {
+    let mut temp_file = tempfile::NamedTempFile::new().unwrap();
+
+    let json_string_request = request.to_json_string();
+
+    let _ = temp_file.write(json_string_request.as_bytes()).unwrap();
+
+    let output = Command::new(executable)
+        .env("DEMO_CONFIG", config_path)
+        .arg("build-tx-info")
+        .arg("claim")
+        .arg("--request")
+        .arg(temp_file.path().to_str().unwrap())
+        .output()
+        .unwrap();
+
+    let stdout_contents = String::from_utf8(output.stdout.clone()).unwrap();
+
+    if !output.status.success() {
+        panic!(
+            "{} failed with stderr: {}",
+            executable,
+            String::from_utf8(output.stderr.clone()).unwrap()
+        );
+    }
+
+    let result = Json::from_json_string(&stdout_contents).unwrap();
+
+    temp_file.close().unwrap();
+
+    result
+}
+
 /// Calls the demo-tx-village CLI to return the eq_validator address
-pub fn eq_validator_address(
+pub fn demo_tx_village_eq_validator_address(
     config_path: &str,
     network: &tx_bakery::chain_query::Network,
 ) -> cardano_serialization_lib::address::Address {
@@ -44,7 +364,7 @@ pub fn eq_validator_address(
 }
 
 /// Calls the demo-tx-village CLI to query the blockchain for UTxOs
-pub fn query_utxos(
+pub fn demo_tx_village_query_utxos(
     config_path: &str,
     network: &tx_bakery::chain_query::Network,
     addr: cardano_serialization_lib::address::Address,
@@ -84,7 +404,7 @@ pub fn query_utxos(
 }
 
 /// Calls the demo-tx-village CLI to build and submit a tx
-pub fn build_and_submit(
+pub fn demo_tx_village_build_and_submit(
     config_path: &str,
     network: &tx_bakery::chain_query::Network,
     signing_key_file: &str,
